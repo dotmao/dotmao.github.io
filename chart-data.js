5049b2e89560ca94c3c581712480e9f2988fb3b7
dotmao <dorothy.hongqian@gmail.com>
2018-06-28 23:12:59 -0400

    add chartjs

--- chart-data.js
+++ chart-data.js
@@ -7,103 +7,97 @@ var chart = new Chart(ctx, {
 
   // The data for our dataset
   data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["Save documents", "Edit and View files", "Organize documents", "Share files", "Other"],
       datasets: [{
           label: "My First dataset",
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'blue',
-            'yellow',
-            'green',
-            'orange',
-            'purple'
+            '#ff6384',
+            '#ddbbdd',
+            '#79F2C4',
+            '#bbddcc',
+            '#ccddaa',
           ],
           // borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
+          data: [20, 14, 14, 1, 1],
       }]
   },
 
   // Configuration options go here
-  options: {}
+  options: {
+    legend: {
+      display: false,
+    }
+  }
 });
 
 // **************chart2*****************
 var ctx2 = document.getElementById('chart2').getContext('2d');
 var chart2 = new Chart(ctx2, {
   // The type of chart we want to create
-  type: 'bar',
+  type: 'doughnut',
 
   // The data for our dataset
   data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["Google Drive", "OneCloud", "DropBox", "iCloud", "Mega"],
       datasets: [{
           label: "My First dataset",
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'blue',
-            'yellow',
-            'green',
-            'orange',
-            'purple'
+            '#79C4F2',
+            '#004466',
+            '#573f7f',
+            '#bdadd8',
+            '#555577',
           ],
           // borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
+          data: [18, 5, 12, 9, 2],
       }]
   },
 
   // Configuration options go here
-  options: {}
+  options: {
+    legend: {
+      display: false,
+    }
+  }
 });
 
 //**************************chart3*******************
 var ctx3 = document.getElementById('chart3').getContext('2d');
 var chart3 = new Chart(ctx3, {
   // The type of chart we want to create
-  type: 'doughnut',
+  type: 'horizontalBar',
 
   // The data for our dataset
   data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["Storage space", "Share content", "Backup files", "Collaboration", "Security and Privacy", "Save from Web"],
       datasets: [{
           label: "My First dataset",
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'blue',
-            'yellow',
-            'green',
-            'orange',
-            'purple'
-          ],
+          backgroundColor: '#add8d9',
           // borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
+          data: [19, 12, 18, 10, 13, 4],
       }]
   },
 
   // Configuration options go here
-  options: {}
+  options: {
+
+  }
 });
 
 //*******************chart4*****************
 var ctx4 = document.getElementById('chart4').getContext('2d');
 var chart4 = new Chart(ctx4, {
   // The type of chart we want to create
-  type: 'bar',
+  type: 'horizontalBar',
 
   // The data for our dataset
   data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["Free trial", "Extra storage", "others"],
       datasets: [{
           label: "My First dataset",
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'blue',
-            'yellow',
-            'green',
-            'orange',
-            'purple'
-          ],
+          backgroundColor: '#ff7f9a',
           // borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
+          data: [9, 3, 3],
       }]
   },
 
